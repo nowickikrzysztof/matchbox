19c45b8d488c2ad9ebc5ea00f70e444b96604cf2
Krzysztof Nowicki <pyrakebab@gmail.com>
2023-05-17 12:13:57 +0200

    Delete unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:matchbox/app/homepage/home_page.dart';
 
 void main() {
